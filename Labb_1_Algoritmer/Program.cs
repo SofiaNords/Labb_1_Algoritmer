f33ec9011840eb4b53d4b514f6ef9e5dafa3d26b
SofiaNords <sofia.k.nordstrom.com>
2024-09-11 07:48:13 +0200

    Add project files.

---
+++ Labb_1_Algoritmer/Program.cs
@@ -0,0 +1,15 @@
+﻿//Console.WriteLine("Mata in en textsträng: ");
+string input = "29535123p48723487597645723645"; //Console.ReadLine();
+
+// Function that checks that a string contains only digits and returns a bool 
+static bool OnlyDigitsInString(string str)
+{
+    foreach(char c in str)
+    {
+        if (!Char.IsDigit(c))
+        {
+            return false;
+        }
+    }
+    return true;
+}
