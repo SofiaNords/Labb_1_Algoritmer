222eb556ba7e5f7f3218217012f1de34607f8290
SofiaNords <sofia.k.nordstrom.com>
2024-09-11 13:36:07 +0200

    Create and test  FindValidSubstrings function

--- Labb_1_Algoritmer/Program.cs
+++ Labb_1_Algoritmer/Program.cs
@@ -1,6 +1,41 @@
 ﻿//Console.WriteLine("Mata in en textsträng: ");
 string input = "29535123p48723487597645723645"; //Console.ReadLine();
 
+// Array of substrings
+string[] substrings = FindValidSubstrings(input);
+
+foreach (string s in substrings)
+{
+    Console.WriteLine(s);
+}
+
+// Function that extracts substrings starting and ending with the same character
+// FindValidSubstrings calls the function AreInnerCharsDifferentFromFirst
+// FindValidSubstrings calls the function ContainsOnlyDigits
+// Returns an array that is validated 
+static string[] FindValidSubstrings(string str)
+{
+    string[] result = new string[str.Length * (str.Length - 1) / 2];
+    int index = 0;
+
+    for (int i = 0; i < str.Length; i++)
+    {
+        for (int j = i + 1; j < str.Length; j++)
+        {
+            if (str[i] == str[j])
+            {
+                string substring = str.Substring(i, j - i + 1);
+                if (AreInnerCharsDifferentFromFirst(substring) && ContainsOnlyDigits(substring))
+                {
+                    result[index++] = substring;
+                }
+            }
+        }
+    }
+    Array.Resize(ref result, index);
+    return result;
+}
+
 
 
 
