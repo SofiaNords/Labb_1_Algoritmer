317f323dcc30c0064b161a335b0574da7f32bdfb
SofiaNords <sofia.k.nordstrom.com>
2024-09-11 08:09:36 +0200

    Create and test AreInnerCharsDifferentFromFirst function

--- Labb_1_Algoritmer/Program.cs
+++ Labb_1_Algoritmer/Program.cs
@@ -1,6 +1,26 @@
 ﻿//Console.WriteLine("Mata in en textsträng: ");
 string input = "29535123p48723487597645723645"; //Console.ReadLine();
 
+
+
+// Function that checks the characters between the first and last character of a given string
+// If any character matches the first character, the function returns false
+// If no characters match the first character, the method returns true
+static bool AreInnerCharsDifferentFromFirst(string str)
+{
+    char firstChar = str[0];
+
+    for (int i = 1; i < str.Length - 1; i++)
+    {
+        if (str[i] == firstChar)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+
 // Function that checks that a string contains only digits and returns a bool 
 static bool OnlyDigitsInString(string str)
 {
@@ -13,7 +33,3 @@ static bool OnlyDigitsInString(string str)
     }
     return true;
 }
-
-bool testDigit = OnlyDigitsInString("123b1345");
-
-Console.WriteLine($"It is: {testDigit}");
